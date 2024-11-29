6c15e8b91e6fdac36e4d9e3f7a581f3214b70a99
Casey Rodarmor <casey@rodarmor.com>
2024-11-29 14:26:06 -0800

    Reform

--- tests/signals.rs
+++ tests/signals.rs
@@ -1,5 +1,6 @@
 // todo:
 // - create issue for sighup handling: can we detect when we receive sighup and are the only recipient?
+// - fix on windows
 // - tests:
 //   - just terminates immediately on receipt of fatal signal if no child process is running
 //   - just reports correct error if child process is terminated with fatal signal and indicates failure
